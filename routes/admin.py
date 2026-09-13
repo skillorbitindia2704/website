adff3f1ba9862dca0807ccbd56bc3881cdfd4002
Rohan Singh <skillorbitindia2704@gmail.com>
2026-09-13 11:06:34 +0530

    Change SKU format to start with 'SO-'

--- routes/admin.py
+++ routes/admin.py
@@ -6077,7 +6077,7 @@ def store_product_create():
 
     #Auot-genrerate SKU only when blank
     if not sku:
-        sku = f"50-(uuid4().hex[:8].upper()}"
+        sku = f"SO-{uuid4().hex[:8].upper()}"
     # Prevent duplicate SKU
     existing_sku = Product.query.filter_by(sku=sku).first()
     
